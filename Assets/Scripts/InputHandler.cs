a5922aa62c4d5bfd7530b15c612b2194021ae352
Giovanni <gio.sales95@gmail.com>
2026-02-16 15:24:40 -0300

    rewired package

--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -1,36 +1,29 @@
+using Rewired;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 // TODO: make inputhandler and compute input, then consume input to move
 public class InputHandler : MonoBehaviour
 {
+    private Player _player;
+    private float _moveSpeed = 10f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        _player = ReInput.players.GetSystemPlayer();
     }
     
     void FixedUpdate()
     {
-        Keyboard keyboard = Keyboard.current;
-        Mouse mouse = Mouse.current;
 
-        if (keyboard != null)
-        {
-            KeyboardInput(keyboard);
-        }
-
+        float moveHorizontal = _player.GetAxis(RewiredConsts.Action.MoveHorizontal);
+        float moveVertical = _player.GetAxis(RewiredConsts.Action.MoveVertical);
         
-    }
-
-    private void KeyboardInput(Keyboard keyboard)
-    {
-        Vector2 movement = Vector2.zero;
-        if (keyboard.wKey.isPressed) movement.y += 1;
-        if (keyboard.sKey.isPressed) movement.y -= 1;
-        if (keyboard.aKey.isPressed) movement.x -= 1;
-        if (keyboard.dKey.isPressed) movement.x += 1;
+        Vector2 movement = new Vector2(moveHorizontal * _moveSpeed, moveVertical * _moveSpeed) * Time.deltaTime;
         
         transform.Translate(movement);
+
+        
     }
 }
