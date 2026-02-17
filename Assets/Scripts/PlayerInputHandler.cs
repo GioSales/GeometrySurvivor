d9bda7d1d4fa9662a6dc67cccebf4fa3c4a7758b
Giovanni <gio.sales95@gmail.com>
2026-02-17 12:52:41 -0300

    firing projectile

---
+++ Assets/Scripts/PlayerInputHandler.cs
@@ -0,0 +1,48 @@
+using Rewired;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+// TODO: make inputhandler and compute input, then consume input to move
+public class PlayerInputHandler : MonoBehaviour
+{
+    private Player _rewiredPlayer;
+    
+    [SerializeField] private float _moveSpeed = 10f;
+
+    [SerializeField] private Weapon _weapon;
+    [SerializeField] private Vector3 mousePosition;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _rewiredPlayer = ReInput.players.GetSystemPlayer();
+    }
+    
+    // TODO: make sure game is not affected by FPS and stays consistent (fixed timestep?)
+    void FixedUpdate()
+    {
+        Movement();
+
+        BasicAttack();
+    }
+
+    private void BasicAttack()
+    {
+        bool basicAttack = _rewiredPlayer.GetButton(RewiredConsts.Action.BasicAttack);
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = Vector2.Normalize(new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y));
+        // Vector2 direction = Vector2.left;
+        if (basicAttack)
+            _weapon.Fire(direction);
+    }
+
+    private void Movement()
+    {
+        float moveHorizontal = _rewiredPlayer.GetAxis(RewiredConsts.Action.MoveHorizontal);
+        float moveVertical = _rewiredPlayer.GetAxis(RewiredConsts.Action.MoveVertical);
+        
+        Vector2 movement = new Vector2(moveHorizontal * _moveSpeed, moveVertical * _moveSpeed) * Time.deltaTime;
+        
+        transform.Translate(movement);
+    }
+}
