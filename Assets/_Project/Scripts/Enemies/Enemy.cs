aec6294c49d3529139a8d003d85e864223242723
Giovanni <gio.sales95@gmail.com>
2026-02-19 22:44:05 -0300

    entitas generated files

---
+++ Assets/_Project/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,35 @@
+using System;
+using UnityEngine;
+
+// TODO: implement spawn system
+// TODO: refactor whole project with ECS
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private int _maxHealth = 100;
+    [SerializeField] private int _currentHealth;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _currentHealth = _maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        _currentHealth -= damage;
+        
+        if (_currentHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log(other.gameObject.layer);
+        if (other.gameObject.layer == LayerMask.NameToLayer("Projectile"))
+        {
+            TakeDamage(50);
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ generate.bat
@@ -0,0 +1,3 @@
+@echo off
+dotnet Jenny\Jenny.Generator.Cli.dll %*
+pause
