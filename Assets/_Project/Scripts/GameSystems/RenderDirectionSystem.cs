3ae5ce5ed308bfa409c674a2e2f00abf43ccdc5f
Giovanni <gio.sales95@gmail.com>
2026-02-21 13:49:28 -0300

    entitas entity spawning and moving test

---
+++ Assets/_Project/Scripts/GameSystems/RenderDirectionSystem.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using Entitas;
+using UnityEngine;
+
+namespace GameSystems
+{
+    public class RenderDirectionSystem : ReactiveSystem<GameEntity>
+    {
+        readonly GameContext _context;
+
+        public RenderDirectionSystem(Contexts contexts) : base(contexts.game)
+        {
+            _context = contexts.game;
+        }
+
+        protected override ICollector<GameEntity> GetTrigger(IContext<GameEntity> context)
+        {
+            return context.CreateCollector(GameMatcher.Direction);
+        }
+
+        protected override bool Filter(GameEntity entity)
+        {
+            return entity.hasDirection && entity.hasView;
+        }
+
+        protected override void Execute(List<GameEntity> entities)
+        {
+            foreach (GameEntity e in entities)
+            {
+                float ang = e.direction.Value;
+                e.view.GameObject.transform.rotation = Quaternion.AngleAxis(ang - 90, Vector3.forward);
+            }
+        }
+    }
+}
