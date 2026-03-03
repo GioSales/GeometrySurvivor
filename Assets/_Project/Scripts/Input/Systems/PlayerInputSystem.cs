3527f044d9b176aad27cede3fb979d25f3957a47
Giovanni <gio.sales95@gmail.com>
2026-03-02 22:32:45 -0300

    Move projectile firing from PlayerActionSystem to PlayerInputSystem
    Inline projectile creation directly in PlayerInputSystem using Rewired
    mouse input instead of reacting to InputEntity events. This simplifies
    the firing flow by reading mouse position and creating projectile entities
    in the same system that handles player input. Remove the now-unused
    PlayerActionSystem reactive system.

--- Assets/_Project/Scripts/Input/Systems/PlayerInputSystem.cs
+++ Assets/_Project/Scripts/Input/Systems/PlayerInputSystem.cs
@@ -1,13 +1,15 @@
 ﻿using Entitas;
 using GamePlayer;
 using Rewired;
+using UnityEngine;
 
 namespace Input.Systems
 {
-    public class PlayerInputSystem : IExecuteSystem
+    public class PlayerInputSystem : IExecuteSystem, IInitializeSystem
     {
         readonly GameContext _context;
         GameEntity _player;
+        Camera _camera;
         
         private readonly Player _rewiredPlayer;
         
@@ -16,6 +18,11 @@ namespace Input.Systems
             _context = contexts.game;
             // TODO: stop using system player, use system player only for debugging actions?
             _rewiredPlayer = ReInput.players.GetSystemPlayer();
+        }
+        
+        public void Initialize()
+        {
+            _camera = Camera.main;
         }
         
         public void Execute()
@@ -30,7 +37,21 @@ namespace Input.Systems
 
             PlayerActionComponent actionComponent = _player.playerAction;
             actionComponent.BasicAtkActive = _rewiredPlayer.GetButton(RewiredConsts.Action.BasicAttack);
-            // TODO: skill action state
+            
+            Mouse mouse = ReInput.controllers.Mouse;
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(mouse.screenPosition);
+            // TODO: cooldown
+            if (actionComponent.BasicAtkActive)
+            {
+                GameEntity projectile = _context.CreateEntity();
+                projectile.AddProjectile(newSpeed: 0.5f);
+                Vector2 direction = mousePosition - _player.position.Value;
+                projectile.AddProjectileDirection(direction);
+                projectile.AddPosition(_player.position.Value);
+                projectile.AddDirection(0);
+                projectile.AddSprite("Capsule");
+                projectile.AddSpriteSize(new Vector3(0.04f, 0.08f, 1));
+            }
         }
     }
 }
